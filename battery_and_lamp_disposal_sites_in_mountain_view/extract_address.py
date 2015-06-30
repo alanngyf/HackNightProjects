ab1fe72216016d14429a9680a0d626dc03799146
Alan Ng <alanngyf@gmail.com>
2015-06-30 02:27:22 -0700

    geocode the addresses and put them directly into a KML file.

--- battery_and_lamp_disposal_sites_in_mountain_view/extract_address.py
+++ battery_and_lamp_disposal_sites_in_mountain_view/extract_address.py
@@ -2,20 +2,25 @@ import urllib2
 import json
 import sys
 import re
+from my_domxml import geocode
+from my_domxml import createKML
+
 data = urllib2.urlopen("http://www.mountainview.gov/depts/pw/recycling/hazard/default.asp")
 page = data.read()
 page = page[page.find("Batteries &amp; Lamps"):page.find("BATTERIES ONLY")]
-address = re.findall("</strong>[^<]+", page)
-for i in range(len(address)):
-	address[i] = re.sub("</strong>", "", address[i])
+addresses = re.findall("</strong>[^<]+", page)
+for i in range(len(addresses)):
+	addresses[i] = re.sub("</strong>", "", addresses[i])
 
 locations = []
-for element in address:
-	print element
-	geom = json.load(urllib2.urlopen("http://maps.googleapis.com/maps/api/geocode/json?address="+urllib2.quote(element)))
+for address in addresses:
+	print address
+	geom = json.load(urllib2.urlopen("http://maps.googleapis.com/maps/api/geocode/json?address="+urllib2.quote(address)))
 	location = geom['results'][0]['geometry']['location']
 	locations.append(location)
 	print location
+	createKML(address, address)
+
 
 # reverse lookup
 # reverse = []
